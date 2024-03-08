c5952fb85c77903f2fd764537707c86cf8453d2c
Layla Maassarani <layla.maassarani@net.usj.edu.lb>
2024-03-08 19:55:28 +0200

    Added the auth module and the countries module to the app module

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
+import { AuthModule } from './auth/auth.module';
+import { CountriesModule } from './countries/countries.module';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
@@ -8,6 +10,8 @@ import { AppComponent } from './app.component';
   imports: [
     AppModule,
     ServerModule,
+    AuthModule,
+    CountriesModule
   ],
   bootstrap: [AppComponent],
 })
