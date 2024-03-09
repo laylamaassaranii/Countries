5ee55dc85337e38e152aaa77edbbe9e392b99658
Layla Maassarani <layla.maassarani@net.usj.edu.lb>
2024-03-09 20:26:39 +0200

    Created the form responsible for the login and its necessary css

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  @ViewChild('firstNameInput') firstNameInput!: ElementRef;
+  @ViewChild('lastNameInput') lastNameInput!: ElementRef;
+  @ViewChild('emailInput') emailInput!: ElementRef;
+  @ViewChild('passwordInput') passwordInput!: ElementRef;
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      admin: [0],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted with data:', this.loginForm.value);
+    }
+  }
 
 }
