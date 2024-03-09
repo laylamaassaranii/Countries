86212c5fcd3d498eb2ef40f0b92e0c94953252dd
Layla Maassarani <layla.maassarani@net.usj.edu.lb>
2024-03-10 01:03:28 +0200

    Added the background images: the world map and the stain, to a shared component

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CountriesRoutingModule } from './countries-routing.module';
 import { CountriesHomeComponent } from './countries-home/countries-home.component';
-
-
+import { WorldMapComponent } from './shared/world-map/world-map.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    CountriesHomeComponent
-  ],
-  imports: [
-    CommonModule,
-    CountriesRoutingModule
-  ]
+  declarations: [CountriesHomeComponent, WorldMapComponent, NavbarComponent],
+  imports: [CommonModule, CountriesRoutingModule],
 })
-export class CountriesModule { }
+export class CountriesModule {}
